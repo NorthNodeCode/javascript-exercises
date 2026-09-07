f992b2c40054efb52caf936a37e06f721349cc30
NorthNodeCode <201049887+NorthNodeCode@users.noreply.github.com>
2026-09-07 12:46:20 +0100

    fixed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,10 @@
 const repeatString = function(string, num) {
-    if (num = 0) {return "ERROR"};
+    if (num < 0) return "ERROR";
     let result = "";
-    for (let i =0; i <=num; i++) {
+    for (let i =0; i < num; i++) {
         result += string;
     }
-    return string;
+    return result;
 };
 
 // Do not edit below this line
